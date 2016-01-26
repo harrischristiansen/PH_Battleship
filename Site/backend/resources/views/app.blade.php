702950d2c65873503fed29b1d54b972ac55335d3
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2016-01-25 21:15:06 -0500

    Added Menubar pages

--- Site/backend/resources/views/app.blade.php
+++ Site/backend/resources/views/app.blade.php
@@ -36,13 +36,15 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">Project name</a>
+					<a class="navbar-brand" href="/">PH Battleship</a>
 				</div>
 				<div id="navbar" class="collapse navbar-collapse">
 					<ul class="nav navbar-nav">
-						<li class="active"><a href="#">Home</a></li>
-						<li><a href="#about">About</a></li>
-						<li><a href="#contact">Contact</a></li>
+						<li><a href="/create_team">Add Team</a></li>
+						<li><a href="/edit_teams">Edit Teams</a></li>
+						<li><a href="/rankings">Rankings</a></li>
+						<li><a href="/manage">Manage Tournament</a></li>
+						<li><a href="/logout">Logout</a></li>
 					</ul>
 				</div>
 			</div>
